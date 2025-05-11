8e5141e67ed398235e5b44ac168d2881185b120f
Fan111i <yify@student.unimelb.edu.au>
2025-05-11 14:05:38 +1000

    using scroll to get all posts for analysis

--- backend/ana.py
+++ backend/ana.py
@@ -1,6 +1,7 @@
 from elasticsearch import Elasticsearch
 from collections import defaultdict
 from datetime import datetime
+import re
 
 es = Elasticsearch(
     "https://elastic:elastic@127.0.0.1:9200",
@@ -12,7 +13,9 @@ es = Elasticsearch(
     retry_on_timeout=True    
 )
 
-all_resp = es.search(
+scroll_size = 10000  # 10000 every time
+scroll_timeout = "2m" 
+scroll_resp = es.search(
     index="socialplatform",
     query={
         "range": {
@@ -22,41 +25,46 @@ all_resp = es.search(
             }
         }
     },
-    track_total_hits=True, 
-    size=10000
+    size=scroll_size,
+    scroll=scroll_timeout
 )
 
-print("match_all ：", all_resp["hits"]["total"]["value"])
+scroll_id = scroll_resp["_scroll_id"]
+total_hits = scroll_resp["hits"]["total"]["value"]
+print(f"Total Posts: {total_hits}")
 
-monthly_tags = defaultdict(lambda: defaultdict(int))
+monthly_keywords = defaultdict(lambda: defaultdict(int))
 
-for data in all_resp["hits"]["hits"]:
-    post = data.get("_source", {})
-    data_section = post.get('data', {})
-    #print(post.get('keywords',{}))
-    #print(data_section['createdAt'])
+while True:
+    for data in scroll_resp["hits"]["hits"]:
+        post = data.get("_source", {})
+        data_section = post.get('data', {})
+
+        created_at = data_section['createdAt']
+        keywords = post.get('keywords', {})
+        
+        if not created_at or not keywords:
+            continue
+
+        try:
+            date_obj = datetime.fromisoformat(created_at[:-1])
+            month_key = date_obj.strftime("%Y-%m")
+        except ValueError:
+            continue
+
+        for keyword in keywords:
+            monthly_keywords[month_key][keyword] += 1
+
+    scroll_resp = es.scroll(scroll_id=scroll_id, scroll=scroll_timeout)
+    if not scroll_resp["hits"]["hits"]:
+        break  
+
+
+print("\n=== Top 5 keywords ===")
+for month, keywords in sorted(monthly_keywords.items()):
+    sorted_keywords = sorted(keywords.items(), key=lambda x: x[1], reverse=True)
+    top_5_keywords = sorted_keywords[:5]
     
-    created_at = data_section['createdAt']
-    keywords = post.get('keywords',{})
-    
-    if not created_at or not keywords:
-        continue
-
-    try:
-        date_obj = datetime.fromisoformat(created_at[:-1])
-        #print(date_obj)
-        month_key = date_obj.strftime("%Y-%m")  # "2023-04"
-    except ValueError:
-        continue
-
-    for keyword in keywords:
-        monthly_tags[month_key][keyword] += 1
-
-print("\n=== Top 5 in every month ===")
-for month, tags in sorted(monthly_tags.items()):
-    sorted_tags = sorted(tags.items(), key=lambda x: x[1], reverse=True)
-    top_5_tags = sorted_tags[:5]
-    
-    print(f"\n{month} top 5 keywords：")
-    for tag, count in top_5_tags:
-        print(f"{tag}: {count} times")
+    print(f"\n{month} top 5")
+    for keyword, count in top_5_keywords:
+        print(f"{keyword}: {count} times")
